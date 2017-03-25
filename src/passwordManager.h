841ac838da471dd79b01c2433c87827a4edf4bdc
holmesfems <holmesfems@gmail.com>
2017-03-25 10:26:25 +0900

    deleted swp

---
+++ src/passwordManager.h
@@ -0,0 +1,21 @@
+/*
+ * passwordManager.h
+ * Created on 2017/03/25 09:53:44
+ * Copyright (C) 2017 zhai <holmesfems@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef _PASSWORDMANAGER_H
+#define _PASSWORDMANAGER_H
+#include<sqlite3.h>
+
+class PasswordManager
+{
+    public:
+        PasswordManager();
+        ~PasswordManager();
+        
+}
+
+#endif // _PASSWORDMANAGER_H
