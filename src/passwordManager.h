7191bb0a9d4627baee0f17db584bc335645c3be0
holmesfems <holmesfems@gmail.com>
2017-03-25 12:02:16 +0900

    add passwordManager.h

--- src/passwordManager.h
+++ src/passwordManager.h
@@ -9,13 +9,28 @@
 #ifndef _PASSWORDMANAGER_H
 #define _PASSWORDMANAGER_H
 #include<sqlite3.h>
-
-class PasswordManager
+#include<string>
+#include<iostream>
+#include<vector>
+namespace PasswordManager
 {
-    public:
-        PasswordManager();
-        ~PasswordManager();
-        
+    class PasswordManager
+    {
+        public:
+            PasswordManager();
+            ~PasswordManager();
+            int openDB(const std::string& filename);
+            std::string searchByDomain(const std::string& domain);
+            std::string searchByIndex(int index);
+            int addPasswd(const std::string& filename,const std::string& domain);
+            int deleteByDomain(const std::string& domain);
+            int deleteByIndex(int index);
+            std::vector<std::string> getDomainLists();
+            std::string getDomainByRegex(const std::string& regexStr);
+
+        private:
+            sqlite3 *conn;
+    };
 }
 
 #endif // _PASSWORDMANAGER_H
