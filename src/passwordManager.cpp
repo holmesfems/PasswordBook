c8ec53a34820bceb277849a006d53df0c6d7af0b
holmesfems <holmesfems@gmail.com>
2017-03-30 13:53:46 +0900

    formatted

--- src/passwordManager.cpp
+++ src/passwordManager.cpp
@@ -83,85 +83,77 @@ namespace PasswordManager
             }
         }
         sqlite3_free_table(query);
-        query=NULL;
+        query = NULL;
         return 0;
-    } //showIndexByDomain()
+    }  // showIndexByDomain()
 
     std::string PasswordManager::searchByIndex(int index)
     {
-        int pnRow,pnCol;
+        int pnRow, pnCol;
         std::ostringstream sql;
         sql << R"(SELECT index,domain,pwdoutput FROM table1 WHERE index=)" << index;
-        char **query = _getQuery(sql.str().c_str(),&pnRow,&pnCol);
-        if(query == NULL){
+        char **query = _getQuery(sql.str().c_str(), &pnRow, &pnCol);
+        if (query == NULL) {
             (*_os) << "An error occured while searching index: " << index << std::endl;
-             return "";
+            return "";
         }
-        std::string ret="";
-        if(pnRow == 0){
+        std::string ret = "";
+        if (pnRow == 0) {
             (*_os) << "No one is hit" << std::endl;
-        }else{
+        } else {
             (*_os) << "Hit " << pnRow << " Results:" << std::endl;
-            int i,j;
-            for(i=0;i<=pnRow;i++)
-            {
-                for(j=0;j<pnCol;j++)
-                {
-                    if(j==2) continue;
-                    if(j>0) (*_os) << '\t';
-                    (*_os) << query[i*pnCol+j];
+            int i, j;
+            for (i = 0; i <= pnRow; i++) {
+                for (j = 0; j < pnCol; j++) {
+                    if (j == 2) continue;
+                    if (j > 0) (*_os) << '\t';
+                    (*_os) << query[i * pnCol + j];
                 }
                 (*_os) << std::endl;
             }
             ret = std::string(query[5]);
         }
         sqlite3_free_table(query);
-        query=NULL;
+        query = NULL;
         return ret;
     }
 
-    int PasswordManager::addPasswd(const std::string &passwdStr,const std::string &domain)
+    int PasswordManager::addPasswd(const std::string &passwdStr, const std::string &domain)
     {
-        int pnRow,pnCol;
+        int pnRow, pnCol;
         std::ostringstream sql;
         sql << R"(SELECT index,domain FROM table1 WHERE domain = ')" << domain << R"(')";
-        char **query = _getQuery(sql.str().c_str(),&pnRow,&pnCol);
-        if(query == NULL){
+        char **query = _getQuery(sql.str().c_str(), &pnRow, &pnCol);
+        if (query == NULL) {
             (*_os) << "An error occured while searching domain: " << domain << std::endl;
-             return -1;
+            return -1;
         }
-        if(pnRow == 0){
+        if (pnRow == 0) {
             sql.str("");
             sql << R"(BEGIN TRANSACTION;)";
             sql << R"(INSERT INTO table1 (domain,pwdoutput) VALUES(')" << domain << R"(',')"
                 << passwdStr << R"(');)";
             sql << R"(COMMIT TRANSACTION;)";
-            char *errMsg=NULL;
-            int err=sqlite3_exec(_conn,sql.str().c_str(),NULL,NULL,&errMsg);
-            if(err!=SQLITE_OK)
-            {
+            char *errMsg = NULL;
+            int err = sqlite3_exec(_conn, sql.str().c_str(), NULL, NULL, &errMsg);
+            if (err != SQLITE_OK) {
                 (*_os) << errMsg << std::endl;
                 return -1;
             }
 
-        }else{
+        } else {
             (*_os) << "Hit " << pnRow << " Results:" << std::endl;
-            int i,j;
-            for(i=0;i<=pnRow;i++)
-            {
-                for(j=0;j<pnCol;j++)
-                {
-                    if(j>0) (*_os) << '\t';
-                    (*_os) << query[i*pnCol+j];
+            int i, j;
+            for (i = 0; i <= pnRow; i++) {
+                for (j = 0; j < pnCol; j++) {
+                    if (j > 0) (*_os) << '\t';
+                    (*_os) << query[i * pnCol + j];
                 }
                 (*_os) << std::endl;
             }
         }
         sqlite3_free_table(query);
-        query=NULL;
+        query = NULL;
         return 0;
-
     }
-
-
 }
